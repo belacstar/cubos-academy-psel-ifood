ebc7fa2b9a8f080823190fab357a31b4efd8da1e
belacstar <belacstar@gmail.com>
2021-08-10 22:30:11 -0300

    Solução Cores

--- cores.js
+++ cores.js
@@ -1,14 +1,23 @@
-function solucao(tamanhoBr) {
-    //seu codigo aqui
-    if (tamanhoBr === "PP") {
-        console.log("XS");
-    } else if (tamanhoBr === "P") {
-        console.log("S");
-    } else if (tamanhoBr === "M") {
-        console.log("M");
-    } else if (tamanhoBr === "G") {
-        console.log("L");
+function solucao(cor1, cor2) {
+	//seu codigo aqui
+    if (cor1 === "azul" && cor2 === "amarelo") {
+        console.log("verde");
+    }  if (cor1 === "amarelo" && cor2 === "azul") {
+        console.log("verde");
+            } if (cor1 === "vermelho" && cor2 === "azul") {
+        console.log("roxo");
+    } if (cor1 === "azul" && cor2 === "vermelho") {
+        console.log("roxo");
+    } if (cor1 === "amarelo" && cor2 === "vermelho") {
+        console.log("laranja");
+        } if (cor1 === "vermelho" && cor2 === "vermelho") {
+        console.log("vermelho");
+        } if (cor1 === "amarelo" && cor2 === "amarelo") {
+        console.log("amarelo");
+            } if (cor1 === "azul" && cor2 === "azul") {
+        console.log("azul");
+                 } if (cor1 === "vermelho" && cor2 === "amarelo") {
+        console.log("laranja");
     } else {
-        console.log("XL");
     }
-  }
+    } 
