531546849cb0eb8fe3b4449831952787da3b83bf
belacstar <belacstar@gmail.com>
2021-08-10 22:37:53 -0300

    Solução Esconde

---
+++ esconde_esconde.js
@@ -0,0 +1,12 @@
+function solucao(numero) {
+	//seu codigo aqui
+   if (numero > 20) { 
+    let x = 21
+while (numero >= x) {
+    console.log(x);
+    x++;
+}
+   } else {
+              console.log("VOCE CONSEGUE, MANINHO");
+    }
+}
